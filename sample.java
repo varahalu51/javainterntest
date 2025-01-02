73389b4b139c0cd6b73e35edb94ae4dc717e7881
varahalu51 <nagam.svb@gmail.com>
2025-01-02 11:57:27 +0530

    add message jan 2nd

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class sample {
     public static void main(String[] args) {
-        System.out.println("This is a sample file");
+        System.out.println("This is a sample file jan 2nd");
     }
 }
